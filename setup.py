5517b23b151a42e138eb4bfe72563550878bbf7e
Jonathan Murga <jonathanm@crehana.com>
2019-04-10 10:52:22 -0500

    upd setup

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ try:
 except ImportError:
     from distutils.core import setup
 
+    def find_packages():
+        return ['redisbeat']
+
 install_requires = [
     "celery == 4.1.0",
     "redis == 2.10.6",
@@ -14,6 +17,7 @@ setup(
     name="redisbeat",
     version="1.0.0",
     author="lamurga",
+    packages=find_packages(),
     download_url="https://github.com/lamurga/crehana-redisbeat",
     description="Python library for crehana",
     install_requires=install_requires
